aeedb742a5eb0773bd97da3d9c0075496fb6afbf
Rakesh Arunachalam <rakesh@thoughtbot.com>
2024-06-21 11:13:13 +0100

    Basic LLM interaction

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello LangChain!");
+import { ChatOllama } from "@langchain/community/chat_models/ollama";
+
+const ollamaLlm = new ChatOllama({
+  baseUrl: "http://localhost:11434",
+  model: "llama3:8b-instruct-q6_K",
+});
+
+const response = await ollamaLlm.invoke("Who created you?");
+console.log(response.content);
