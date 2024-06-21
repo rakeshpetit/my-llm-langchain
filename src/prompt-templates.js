e814143a10b0602b3102f3c6c5d1dc0130d7931e
Rakesh Arunachalam <rakesh@thoughtbot.com>
2024-06-21 12:56:05 +0100

    Add a second example template

--- src/prompt-templates.js
+++ src/prompt-templates.js
@@ -5,4 +5,11 @@ const promptTemplate = ChatPromptTemplate.fromMessages([
   ["user", "Tell me a joke about {topic}"],
 ]);
 
+const shortDescription = ChatPromptTemplate.fromMessages([
+  ["system", "You are a helpful assistant."],
+  ["user", "Tell me something about {topic} in 50 words"],
+]);
+
+export { shortDescription };
+
 export default promptTemplate;
