446620fbe219282fef89087eb08cd563801aa27b
Rakesh Arunachalam <rakesh@thoughtbot.com>
2024-06-21 12:04:10 +0100

    System and User templates

--- src/prompt-templates.js
+++ src/prompt-templates.js
@@ -1,7 +1,8 @@
 import { ChatPromptTemplate } from "@langchain/core/prompts";
 
-const promptTemplate = ChatPromptTemplate.fromTemplate(
-  "Tell me a joke about {topic}"
-);
+const promptTemplate = ChatPromptTemplate.fromMessages([
+  ["system", "You are a helpful assistant."],
+  ["user", "Tell me a joke about {topic}"],
+]);
 
 export default promptTemplate;
